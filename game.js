65c34ebd4b091ad453ddfbc8680b359784d883b0
allegamboa <55117006+allegamboa@users.noreply.github.com>
2019-09-11 13:33:08 -0600

    Create game.js

---
+++ game.js
@@ -0,0 +1,13 @@
+var button = document.getElementById("clickAqui"),
+  count = 100;
+button.onclick = function() {
+  count -=1;
+  button.innerHTML = "Click Aqui: " + count;
+};
+
+let clicks = 0;
+while (count < 100 ) {
+count = count   +1;
+}
+
+alert ("ganaste codigo 2B5789");
